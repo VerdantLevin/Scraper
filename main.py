9d9120cece5d8710bccd3532f881c2ab760521f2
VerdantLevin <phamanhduc7025@gmail.com>
2024-01-15 21:03:28 +0700

    Web + DB Update

--- main.py
+++ main.py
@@ -3,11 +3,58 @@ from bs4 import BeautifulSoup
 import re
 import matplotlib.pyplot as plt
 from flask import Flask, render_template
+import json
+import sqlite3
+
 
 app = Flask(__name__)
 app.secret_key = "vevdvwfwefw".encode("utf8")
 app.template_folder = "templates"
 app.static_folder = "static"
+
+def create_database():
+    with sqlite3.connect('hotels.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS hotels (
+                id INTEGER PRIMARY KEY,
+                ten_ks TEXT,
+                so_sao TEXT,
+                danh_gia TEXT,
+                dia_chi TEXT
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS binh_luan (
+                id INTEGER PRIMARY KEY,
+                hotel_id INTEGER,
+                ho_ten TEXT,
+                ngay_thang TEXT,
+                cam_nhan_kh TEXT,
+                nhan_xet TEXT,
+                so_sao_dg TEXT,
+                FOREIGN KEY(hotel_id) REFERENCES hotels(id)
+            )
+        ''')
+
+        conn.commit()
+
+
+def insert_data_into_db(hotel):
+    with sqlite3.connect('hotels.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO hotels (ten_ks, so_sao, danh_gia, dia_chi) VALUES (?, ?, ?, ?)",
+                       (hotel.ten_ks, hotel.so_sao, hotel.danh_gia, hotel.dia_chi))
+        hotel_id = cursor.lastrowid
+
+        for binh_luan in hotel.binh_luan:
+            cursor.execute(
+                "INSERT INTO binh_luan (hotel_id, ho_ten, ngay_thang, cam_nhan_kh, nhan_xet, so_sao_dg) VALUES (?, ?, "
+                "?, ?, ?, ?)",
+                (hotel_id, binh_luan.ho_ten, binh_luan.ngay_thang, binh_luan.cam_nhan_kh, binh_luan.nhan_xet,
+                 binh_luan.so_sao_dg))
+        conn.commit()
 
 class BinhLuan:
     def __init__(self, ho_ten, ngay_thang, cam_nhan_kh, nhan_xet, so_sao_dg):
@@ -89,6 +136,17 @@ def get_pagination(url):
     # If there's no pagination element, return an empty list
     return []
     
+    
+def convert_to_dict(obj):
+    if isinstance(obj, BinhLuan):
+        return obj.__dict__
+    raise TypeError(f"Không thể chuyển đổi {obj} sang từ điển.")
+
+
+def save_to_json(data):
+    with open("data.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, default=convert_to_dict, ensure_ascii=False, indent=4)
+
 # Summarize hotels by score
 def summarize_by_score():
     scores = {}
@@ -178,13 +236,14 @@ def index():
     return render_template("index.html", hotels = hotels)
     
 if __name__ == "__main__":
+    create_database()
     url1 = "https://khachsan.chudu24.com/t.hanoi.html"
     so_trang = get_pagination(url1)
-    # Call the function
+    hotels = []
     for st in so_trang:
         crawl(st, "https://khachsan.chudu24.com/t.hanoi.html?page=")
-    hotels = sorted(hotels, key=lambda hotel: int(hotel.danh_gia), reverse=True)
-    #summarize_by_score()
-    #summarize_by_reviews()
-    #summarize_by_districts()
+    for hotel in hotels:
+        insert_data_into_db(hotel)
+    hotels_data = [hotel.__dict__ for hotel in hotels]
+    save_to_json(hotels_data)
     app.run()
